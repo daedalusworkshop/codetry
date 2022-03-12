bbc99b10a45b099e4e8d348d531147d351c0650b
Daedalus <k2mikaili@gmail.com>
2022-03-12 15:52:19 -0600

    chasm from folder to file

--- chasm.py
+++ chasm.py
@@ -1,7 +1,9 @@
-def lightningbolt(left,right,end,s) :
+def zeus(left,right,end,s) :
   for _ in range(s+1):
     if s == 0 :
       print(end)
     else :
       print(left + " " * s + right)
       s -= 1
+
+zeus("god", "man", "mad", 10)
